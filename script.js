2a6c9c19dc2e32dc0dfc5020a10ddcd936275551
wael-abd-ALWHAB <139469407+wael-abd-ALWHAB@users.noreply.github.com>
2024-06-27 03:41:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ function playSurah(src) {
     const mainContent = document.getElementById('main-content')
     const audioContainer = document.getElementById('audio-container');
     const audioPlayer = document.getElementById('audioPlayer');
+    const currentSurah = document.getElementById('current-surah')
     audioPlayer.src = src;
     audioPlayer.play();
     mainContent.style.display = 'none'
     audioContainer.style.display = 'block';
+    currentSurah.style.display = 'block'
 }
